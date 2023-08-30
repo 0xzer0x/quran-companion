ffab283e7b7b8109e9496492d6103edb237b80f0
0xzer0x <32334265+0xzer0x@users.noreply.github.com>
2023-08-30 15:41:03 +0300

    DBManager documentation added

--- src/core/mainwindow.h
+++ src/core/mainwindow.h
@@ -41,7 +41,7 @@ class MainWindow : public QMainWindow
   Q_OBJECT
 
 public:
-  MainWindow(QWidget* parent = nullptr);
+  explicit MainWindow(QWidget* parent = nullptr);
   ~MainWindow();
 
   void highlightCurrentVerse();
