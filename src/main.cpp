61fc2ab176957e410db7c9c010276f664ac63377
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-01-03 23:37:28 +0200

    v1.2.1 bugfixes

--- src/main.cpp
+++ src/main.cpp
@@ -108,7 +108,7 @@ main(int argc, char* argv[])
   QApplication a(argc, argv);
   QApplication::setApplicationName("Quran Companion");
   QApplication::setOrganizationName("0xzer0x");
-  QApplication::setApplicationVersion("1.2.0");
+  QApplication::setApplicationVersion("1.2.1");
 
   QSplashScreen splash(QPixmap(":/resources/splash.png"));
   splash.show();
