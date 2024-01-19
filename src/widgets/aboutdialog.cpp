1f5e476c437dc4c55af2a1d27a339fac173084eb
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-01-19 17:06:58 +0200

    UI: Hide remove html elements in AboutDialog translations

--- src/widgets/aboutdialog.cpp
+++ src/widgets/aboutdialog.cpp
@@ -8,6 +8,13 @@ AboutDialog::AboutDialog(QWidget* parent)
   ui->setupUi(this);
   ui->translatorsTab->setLayoutDirection(Qt::LeftToRight);
   ui->lbVersionNum->setText(qApp->applicationVersion());
+  ui->lbLicense->setText(ui->lbLicense->text().arg(
+    tr("Licensed under the"), tr("Waqf General Public License")));
+  ui->lbHomepage->setText(ui->lbHomepage->text().arg(tr("Project Homepage")));
+  ui->lbBugFeatureReq->setText(
+    ui->lbBugFeatureReq->text().arg(tr("Report a bug/Request a feature")));
+  ui->lbContribTranslation->setText(
+    ui->lbContribTranslation->text().arg(tr("Contribute to translations")));
   connect(ui->btnBox, &QDialogButtonBox::accepted, this, &QDialog::accept);
   if (m_lang == QLocale::Arabic)
     ui->aboutTabWidget->setObjectName("rtlTabWidget");
