6aec73665d0b4d57faa2ee94a18e3bb4ec4d6092
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-05-22 11:41:20 +0300

    refactor: add khatmah service and sql implementation

---
+++ src/service/khatmahservice.h
@@ -0,0 +1,65 @@
+#ifndef KHATMAHSERVICE_H
+#define KHATMAHSERVICE_H
+
+#include <types/verse.h>
+
+class KhatmahService
+{
+public:
+  /**
+   * MODIFIED
+   */
+  virtual std::optional<Verse> loadVerse(const int khatmahId) const = 0;
+  /**
+   * @brief sets the given Verse as the last position reached in the current
+   * active khatmah
+   * @param v - Verse reached in khatmah
+   */
+  virtual bool saveActiveKhatmah(const Verse& verse) = 0;
+  /**
+   * @brief get all available khatmah ids
+   * @return QList of khatmah id(s)
+   */
+  virtual QList<int> getAllKhatmah() const = 0;
+  /**
+   * @brief get the name of the khatmah with id given
+   * @return QString containing the khatmah name
+   */
+  virtual QString getKhatmahName(const int id) const = 0;
+  /**
+   * @brief add a new khatmah/replace khatmah with given id with position of
+   * Verse v
+   * @param v - Verse to set as the khatmah position
+   * @param name - new khatmah name
+   * @param id - id of khatmah to replace, -1 means do not replace (default: -1)
+   * @return id of newly added khatmah or id parameter if defined
+   */
+  virtual int addKhatmah(const Verse& verse,
+                         const QString name,
+                         const int id = -1) const = 0;
+  /**
+   * @brief rename the khatmah with the given id to newName
+   * @param khatmahId - id of khatmah to rename
+   * @param newName - new name to set
+   * @return boolean indicating a successful operation (false in case the name
+   * exists)
+   */
+  virtual bool editKhatmahName(const int khatmahId, QString newName) = 0;
+  /**
+   * @brief remove the khatmah with the given id from database
+   * @param id - id of khatmah to remove
+   */
+  virtual void removeKhatmah(const int id) const = 0;
+  /**
+   * @brief setter for m_activeKhatmah
+   * @param id - id of the active khatmah
+   */
+  virtual void setActiveKhatmah(const int id) = 0;
+  /**
+   * @brief getter for m_activeKhatmah
+   * @return int - id of active khatmah
+   */
+  virtual int activeKhatmah() const = 0;
+};
+
+#endif // KHATMAHSERVICE_H
