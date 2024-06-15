9496f95d42bf501e0a12bbe39f0b3843b1c1e931
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-06-15 17:37:13 +0300

    refactor: rewrite page size calculation

--- src/widgets/quranpagebrowser.cpp
+++ src/widgets/quranpagebrowser.cpp
@@ -119,10 +119,7 @@ QuranPageBrowser::setHref(QTextCursor* cursor, int to, QString url)
 void
 QuranPageBrowser::insertFooter(QTextCursor* cursor, int page)
 {
-  if (m_config.qcfVersion() == 1)
-    m_pageInfoTextFormat.setFontPointSize(m_fontSize - 4);
-  else
-    m_pageInfoTextFormat.setFontPointSize(m_fontSize - 6);
+  m_pageInfoTextFormat.setFontPointSize(m_fontSize - 6);
 
   cursor->insertBlock(m_pageFormat, m_pageInfoTextFormat);
   QFontMetrics fm(m_pageInfoTextFormat.font());
@@ -402,19 +399,12 @@ int
 QuranPageBrowser::bestFitFontSize()
 {
   int sz;
-  int margin = 50;
-  static QRegularExpression rem("frame.*|bsml");
+  int margin = 10;
   for (sz = 28; sz >= 12; sz--) {
-    QFont pf(m_pageFont, sz);
-    QFontMetrics fm(pf);
+    QFontMetrics pageMetrics(QFont(m_pageFont, sz));
     QFontMetrics headerMetrics(QFont("PakType Naskh Basic", sz - 6));
-    QString completePage = m_currPageLines.join('\n');
-    completePage.remove(rem);
-
-    QSize textSz =
-      headerMetrics.size(Qt::TextSingleLine, m_currHeaderSegments.join(' ')) +
-      fm.size(0, completePage);
-    if (textSz.height() + margin <= parentWidget()->height())
+    int pageHeight = (pageMetrics.height() * 15) + (headerMetrics.height() * 2);
+    if (pageHeight + margin <= parentWidget()->height())
       break;
   }
 
