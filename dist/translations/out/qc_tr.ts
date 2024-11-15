9f64ac8571d24a81ea1df43aff9f619dc458174c
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-11-15 14:35:50 +0200

    crowdin: update Turkish translation

--- dist/translations/out/qc_tr.ts
+++ dist/translations/out/qc_tr.ts
@@ -657,8 +657,8 @@
     </message>
     <message>
       <location filename="../../../src/dialogs/settingsdialog.ui" line="292"/>
-      <location filename="../../../src/dialogs/settingsdialog.ui" line="415"/>
-      <location filename="../../../src/dialogs/settingsdialog.ui" line="538"/>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="420"/>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="543"/>
       <source>size</source>
       <translation type="unfinished">size</translation>
     </message>
@@ -674,32 +674,37 @@
     </message>
     <message>
       <location filename="../../../src/dialogs/settingsdialog.ui" line="400"/>
-      <source>Uthmanic</source>
-      <translation type="unfinished">Uthmanic</translation>
+      <source>Hafs</source>
+      <translation type="unfinished">Hafs</translation>
     </message>
     <message>
       <location filename="../../../src/dialogs/settingsdialog.ui" line="405"/>
-      <source>Uthmanic (annotated)</source>
-      <translation type="unfinished">Uthmanic (annotated)</translation>
+      <source>Hafs (annotated)</source>
+      <translation type="unfinished">Hafs (annotated)</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/settingsdialog.ui" line="505"/>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="410"/>
+      <source>Warsh</source>
+      <translation type="unfinished">Warsh</translation>
+    </message>
+    <message>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="510"/>
       <source>Side content</source>
       <translation>Yan içerik</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/settingsdialog.ui" line="513"/>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="518"/>
       <source>Font Family</source>
       <translation>Yazı Tipi Ailesi</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/settingsdialog.ui" line="624"/>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="629"/>
       <location filename="../../../src/dialogs/downloaderdialog.cpp" line="130"/>
       <source>Translation</source>
       <translation>Çeviri</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/settingsdialog.ui" line="647"/>
+      <location filename="../../../src/dialogs/settingsdialog.ui" line="652"/>
       <source>Shortcuts</source>
       <translation>Kısayollar</translation>
     </message>
@@ -806,27 +811,27 @@
       <translation type="unfinished">Translators</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="369"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="406"/>
       <source>Credits</source>
       <translation type="unfinished">Credits</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="390"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="427"/>
       <source>Recitations</source>
       <translation type="unfinished">Recitations</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="438"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="475"/>
       <source>Tafsir/Translations</source>
       <translation type="unfinished">Tafsir/Translations</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="489"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="526"/>
       <source>Surah Cards</source>
       <translation type="unfinished">Surah Cards</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="535"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="572"/>
       <source>Libraries</source>
       <translation type="unfinished">Libraries</translation>
     </message>
