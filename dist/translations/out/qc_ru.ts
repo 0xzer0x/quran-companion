9d65b80ade38731084ad292418f10abc7cbdf187
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2024-11-15 22:32:42 +0200

    crowdin: update Russian translation

--- dist/translations/out/qc_ru.ts
+++ dist/translations/out/qc_ru.ts
@@ -826,22 +826,27 @@
       <translation>Авторы</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="427"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="424"/>
+      <source>Quran Text</source>
+      <translation type="unfinished">Quran Text</translation>
+    </message>
+    <message>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="473"/>
       <source>Recitations</source>
       <translation>Чтецы</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="475"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="521"/>
       <source>Tafsir/Translations</source>
       <translation>Тафсир/Переводы</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="526"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="572"/>
       <source>Surah Cards</source>
       <translation>Карточки Сур</translation>
     </message>
     <message>
-      <location filename="../../../src/dialogs/aboutdialog.ui" line="572"/>
+      <location filename="../../../src/dialogs/aboutdialog.ui" line="618"/>
       <source>Libraries</source>
       <translation>Библиотеки</translation>
     </message>
