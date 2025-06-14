313affe1e227461a2bab8796dddb39c9f1a2b416
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-06-14 16:14:02 +0300

    crowdin: update Spanish translation

--- dist/translations/out/qc_es.ts
+++ dist/translations/out/qc_es.ts
@@ -1364,7 +1364,7 @@
     <message>
       <location filename="../../../src/widgets/repeaterpopup.ui" line="212"/>
       <source>Post-verse pause (sec)</source>
-      <translation type="unfinished">Post-verse pause (sec)</translation>
+      <translation>Pausa post-versículo (seg)</translation>
     </message>
     <message>
       <location filename="../../../src/widgets/repeaterpopup.ui" line="226"/>
@@ -1379,7 +1379,7 @@
     <message>
       <location filename="../../../src/widgets/repeaterpopup.cpp" line="134"/>
       <source>Verse frequency and repetitions must be more than 0</source>
-      <translation type="unfinished">Verse frequency and repetitions must be more than 0</translation>
+      <translation>La frecuencia del versículo y las repeticiones deben ser más de 0</translation>
     </message>
   </context>
   <context>
